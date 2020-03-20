d45ee2224241b1c75aa71f2bf85bd6d5d387f173
ooklwq <ooklwq@163.com>
2020-03-20 23:13:35 +0800

    向数组中添加元素

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -11,4 +11,36 @@ public class Array {
     public Array(){
         this(10);
     }
+
+    //向索引index处添加元素e
+    public void add(int index, int e){
+        if (size == data.length){
+            throw new IllegalArgumentException("Add failed.Array is full");
+        }
+        if(index<0 || index >size){
+            throw new IllegalArgumentException("index needs to be index>0 &&index <=size");
+        }
+        for(int i = size -1 ; i>=index; i--){
+            data[i+1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    public void addLast(int e){
+        add(size,e);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+        str.append(String.format("size:%d,capacity:%d\n",size,data.length ));
+        str.append("[");
+        for (int i = 0; i < size; i++){
+            str.append(data[i]);
+            if (i != size-1) str.append(", ");
+        }
+        str.append("]");
+        return str.toString();
+    }
 }
--- Array/src/Main.java
+++ Array/src/Main.java
@@ -1,6 +1,17 @@
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//	    int[] arr = new int[10];
+//	    arr[0] = 1;
+//	    System.out.println(arr[0]);
+        Array arr = new Array(10);
+        for(int i=0;i<8;i++){
+            arr.addLast(i);
+        }
+        System.out.println(arr.toString());
+        arr.addLast(10);
+        System.out.println(arr.toString());
     }
 }
