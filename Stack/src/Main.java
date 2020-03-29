f4da0f0b30c4d8c31b74fb228d174a4fb8e772ef
ooklwq <ooklwq@163.com>
2020-03-29 22:32:00 +0800

    使用链表实现栈

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,11 +1,24 @@
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        ArrayStack<Integer> stack = new ArrayStack<>();
-        for (int i = 0; i < 5; i++) {
-            stack.push(i);
-            System.out.println(stack);
+    private static double testStack(Stack stack, int optCount){
+        Random random = new Random();
+        long startTime = System.nanoTime();
+        for (int i = 0; i < optCount; i++) {
+            stack.push(random.nextInt(Integer.MAX_VALUE));
         }
-        stack.pop();
-        System.out.println(stack);
+        for (int i = 0; i < optCount; i++) {
+            stack.pop();
+        }
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+        int optCount = 100000;
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        System.out.println("ArrayStack: "+testStack(arrayStack, optCount));
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        System.out.println("LinkedListStack: "+testStack(linkedListStack, optCount));
     }
 }
