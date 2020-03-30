c4d40d4889c82dc4d353f4dcf3354f92fc1c0589
ooklwq <ooklwq@163.com>
2020-03-30 13:44:21 +0800

    使用链表实现队列

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Date;
 import java.util.Random;
 
 public class Main {
@@ -15,11 +14,14 @@ public class Main {
         long endTime = System.nanoTime();
         return (endTime - startTime) / 1000000000.0;
     }
+
     public static void main(String[] args) {
         int optCount = 100000;
         ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
         System.out.println("ArrayQueue: "+testQueue(arrayQueue, optCount)+"s");
         LoopQueue<Integer> loopQueue = new LoopQueue<>();
         System.out.println("LoopQueue: "+testQueue(loopQueue, optCount)+"s");
+        LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<>();
+        System.out.println("LinkedListQueue: "+testQueue(linkedListQueue, optCount)+"s");
     }
 }
