565047f364224e01c5d504d804827f6e9b16df2c
ooklwq <ooklwq@163.com>
2020-04-25 16:57:39 +0800

    链表实现Set

--- Set/src/LinkedListSet.java
+++ Set/src/LinkedListSet.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+public class LinkedListSet<E> implements Set<E>{
+    private LinkedList<E> link;
 
-public class LinkedListSet {
+    public LinkedListSet(){
+        link = new LinkedList<>();
+    }
+
+    @Override
+    public void add(E e) {
+        if (!link.contains(e))
+            link.addFirst(e);
+    }
+
+    @Override
+    public void remove(E e) {
+        link.removeElement(e);
+    }
+
+    @Override
+    public int getSize() {
+        return link.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return link.isEmpty();
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return link.contains(e);
+    }
 }
